d3064b49fa5a0c0613641673dd1d118e09994e85
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-19 10:13:56 +1100

    feat: implement small calculator day 1 part 1 solution

--- src/main/java/adventofcode2022/day1/Main1.java
+++ src/main/java/adventofcode2022/day1/Main1.java
@@ -9,12 +9,16 @@ public class Main1 {
     public static void main(String[] args) throws IOException {
         String filePath = "adventofcode2022/day1/input.txt";
         String fileContents = FileLoader.getFileAsString(filePath);
-        System.out.println(fileContents);
-
+//        System.out.println(fileContents);
+//
         BufferedReader reader = FileLoader.getFileAsReader(filePath);
 
 //        LargeCalorieCounter should be used if input file greater than 1GB
-        int fattestElfCalories = LargeCalorieCounter.findElfWithMostCalories(reader);
-        System.out.println(fattestElfCalories);
+        int largeSolution = LargeCalorieCounter.findElfWithMostCalories(reader);
+        System.out.println("Large calculator solution: " + largeSolution);
+
+        int smallSolution = SmallCalorieCounter.findElfWithMostCalories(fileContents);
+        System.out.println("Small calculator solution: " + smallSolution);
+
     }
 }
--- src/main/java/adventofcode2022/day1/SmallCalorieCounter.java
+++ src/main/java/adventofcode2022/day1/SmallCalorieCounter.java
@@ -1,7 +1,17 @@
 package adventofcode2022.day1;
 
 
+import java.util.Arrays;
+
+
 public class SmallCalorieCounter {
 
-//    TODO
+    public static int findElfWithMostCalories(String input) {
+        String[] blocks = input.split("\\n\\n");
+        return Arrays.stream(blocks).mapToInt(SmallCalorieCounter::getElfSum).max().orElse(0);
+    }
+
+    private static int getElfSum(String elfBlock) {
+        return elfBlock.lines().mapToInt(Integer::valueOf).sum();
+    }
 }
