e4fe326cd98043b9be71083dba62a0db0f8b4f63
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-19 10:31:49 +1100

    chore: add comment

--- src/main/java/adventofcode2022/day1/SmallCalorieCounter.java
+++ src/main/java/adventofcode2022/day1/SmallCalorieCounter.java
@@ -3,7 +3,7 @@ package adventofcode2022.day1;
 
 import java.util.Arrays;
 
-
+// Small calculator used if input file is small (< 1GB)
 public class SmallCalorieCounter {
 
     public static int findElfWithMostCalories(String input) {
