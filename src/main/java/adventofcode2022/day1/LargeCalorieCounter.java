a3f29b78b16f9ad2f0889d7c458b34a1fac3f0f3
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-19 20:17:28 +1100

    feat: add test

--- src/main/java/adventofcode2022/day1/LargeCalorieCounter.java
+++ src/main/java/adventofcode2022/day1/LargeCalorieCounter.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 // Large calculator is used if input file is large (> 1GB)
 public class LargeCalorieCounter {
--- src/test/java/adventofcode2022/day1/LargeCalorieCounterTest.java
+++ src/test/java/adventofcode2022/day1/LargeCalorieCounterTest.java
@@ -10,23 +10,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargeCalorieCounterTest {
 
-    static final String input ="""
-        1000
-        1000
-        1000
-        
-        50
-        300
-        5
-        
-        400
-
-        0
-        """;
-    static final BufferedReader TEST_INPUT = new BufferedReader(new StringReader(input));
+    static final String testString = """
+            100
+            200
+            300
+            
+            400
+            500
+            600
+            
+            700
+            800
+            900
+            
+            10
+            20
+            30
+            """;
+    static final BufferedReader TEST_INPUT = new BufferedReader(new StringReader(testString));
 
     @Test
-    void givenSampleInput_findElfWithMostCalories_returns3000() throws IOException {
-        assertEquals(3000, LargeCalorieCounter.findElfWithMostCalories(TEST_INPUT));
+    void givenSampleInput_findElfWithMostCalories_returns2400() throws IOException {
+        assertEquals(2400, LargeCalorieCounter.findElfWithMostCalories(TEST_INPUT));
+    }
+
+    @Test
+    void givenSampleInput_findSumOfTopThreeElves_returns4500() throws IOException {
+        assertEquals(4500, LargeCalorieCounter.findSumOfTopThreeElves(TEST_INPUT));
     }
 }
