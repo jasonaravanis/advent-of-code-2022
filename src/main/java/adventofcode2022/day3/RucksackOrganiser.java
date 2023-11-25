c20fcee803e86d390fb48057ee597202956d7af3
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-25 16:00:56 +1100

    feat: refactor day 3 part 1 solution

--- src/main/java/adventofcode2022/day3/RucksackOrganiser.java
+++ src/main/java/adventofcode2022/day3/RucksackOrganiser.java
@@ -1,20 +1,13 @@
 package adventofcode2022.day3;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class RucksackOrganiser {
 
-    public static int getSumOfItemPriorities(BufferedReader reader) throws IOException {
-        String line;
-        ArrayList<Rucksack> rucksacks = new ArrayList<>();
-        while ((line = reader.readLine()) != null) {
-            rucksacks.add(new Rucksack(line));
-        }
-        return rucksacks
-                .stream()
+    public static int getSumOfItemPriorities(BufferedReader reader) {
+        return reader.lines()
+                .map(Rucksack::new)
                 .map(Rucksack::getMisplacedItem)
                 .filter(Objects::nonNull)
                 .map(item -> item.priority)
